56678c59e7a23b602c940893b4ea9e742ad402de
ET <tarakanova_ekaterina@hotmail.com>
2023-11-11 11:47:00 +0300

    First commit

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const sign = ref(null)
+</script>
+
+<template>
+  <section class="input-area">
+    <label class="input-area__input">
+      <span>Enter sign of arifmetical operation</span>
+      <input type="text" id="number1" v-model="sign" />
+    </label>
+  </section>
+</template>
+
+<style scoped>
+.input-area {
+  display: flex;
+  flex-direction: column;
+  row-gap: 20px;
+}
+
+.input-area__input {
+  display: flex;
+  column-gap: 25px;
+}
+</style>
